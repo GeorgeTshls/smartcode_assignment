96a58e535fa6245c030bbe74008c7533f88dcd78
gtsouchlis <george.tsouchlis@gmail.com>
2023-03-27 20:02:16 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     )
   }
 
-  const UserList = ({users})=> {
+  const UserList = ({ users }) => {
 
     function handleDelete(user) {
       const updatedUsers = users.filter((u) => u.email !== user.email);
@@ -48,8 +48,8 @@ function App() {
             <div>
               <p className='userlist_text'>{user.name}</p>
               <p className='userlist_text'>{user.email}</p>
-              <p className='userlist_text'>{'Check-In: '+moment(user?.checkin).format('DD/MM/YYYY')}</p>
-              <p className='userlist_text'>{'Check-Out: '+moment(user?.checkout).format('DD/MM/YYYY')}</p>
+              <p className='userlist_text'>{'Check-In: ' + moment(user?.checkin).format('DD/MM/YYYY')}</p>
+              <p className='userlist_text'>{'Check-Out: ' + moment(user?.checkout).format('DD/MM/YYYY')}</p>
             </div>
 
             <FontAwesomeIcon icon={faTrashAlt} className='trash_icon' onClick={() => handleDelete(user)} />
@@ -64,7 +64,7 @@ function App() {
     document.getElementById('calendar_cont').style.opacity = 1;
   }
 
-  function closeCalendar(){
+  function closeCalendar() {
     document.getElementById('main').style.opacity = 1;
     document.getElementById('calendar_cont').style.opacity = 0;
     setName('');
@@ -77,36 +77,44 @@ function App() {
     setResetKey(Math.random())
   }
 
-  function addUser(){
-    let check=true;
-    users?.forEach((usr)=>{
-      if(usr?.name === name) check=false;
+  function addUser() {
+    let check = true;
+    users?.forEach((usr) => {
+      if (usr?.checkout !== usr?.checkin) {
+        if (moment(selection?.endDate).isBetween(moment(usr?.checkin), moment(usr?.checkout)) || moment(selection?.startDate).isBetween(moment(usr?.checkin), moment(usr?.checkout))) {
+          check = false;
+        } else {
+          if (moment(selection?.startDate).isBetween(moment(usr?.checkin), moment(usr?.checkout))) {
+            check = false;
+          }
+        }
+      }
     })
 
-    if(!check){
-      window.alert('Reservation on this name already exists!')
+    if (!check) {
+      window.alert('There are already bookings on the selected dates!')
       return;
     }
 
-    if(name !== '' && nights>0){
+    if (name !== '' && nights > 0) {
       axios.get('https://randomuser.me/api/?results=1')
-      .then((response) => {
-        let arr = users;
-        arr.push({
-          name: name,
-          checkout: selection.endDate,
-          checkin: selection.startDate,
-          email: response.data.results[0]?.email,
+        .then((response) => {
+          let arr = users;
+          arr.push({
+            name: name,
+            checkout: selection.endDate,
+            checkin: selection.startDate,
+            email: response.data.results[0]?.email,
+          });
+          setUsers(arr);
+          closeCalendar();
+        })
+        .catch((error) => {
+          window.alert(error.message);
+          closeCalendar();
         });
-        setUsers(arr);
-        closeCalendar();
-      })
-      .catch((error) => {
-        window.alert(error.message);
-        closeCalendar();
-      });
 
-    }else{
+    } else {
       window.alert('You need to enter a name & a date')
     }
   }
@@ -115,8 +123,8 @@ function App() {
     const { selection } = ranges;
     if (selection) {
       setSelection(selection);
-      const days = differenceInDays(selection.endDate, selection.startDate)+1;
-      setNights(days); 
+      const days = differenceInDays(selection.endDate, selection.startDate) + 1;
+      setNights(days);
     };
   }
 
@@ -125,13 +133,13 @@ function App() {
     <div className="App">
       <div id='main'>
         <Header />
-        <UserList users={users}/>
+        <UserList users={users} />
       </div>
       <div id='calendar_cont'>
         <p className='header_title'>Add reservation</p>
         <div className='row_cont input_cont'>
           <p>Name</p>
-          <input onChange={(e)=>setName(e.target.value)} value={name}></input>
+          <input onChange={(e) => setName(e.target.value)} value={name}></input>
         </div>
         <DateRange
           ranges={[selection]}
@@ -151,8 +159,8 @@ function App() {
         </div>
 
         <div className='row_cont'>
-          <button onClick={()=>addUser()} style={{backgroundColor:'#69dbdb'}}>Add</button>
-          <button onClick={()=>closeCalendar()}>Cancel</button>
+          <button onClick={() => addUser()} style={{ backgroundColor: '#69dbdb' }}>Add</button>
+          <button onClick={() => closeCalendar()}>Cancel</button>
         </div>
       </div>
 
